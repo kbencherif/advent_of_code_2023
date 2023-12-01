8796c3eacc2be4698c8209f6605881da866e4c45
Kamel <kbencherif29@gmail.com>
2023-12-01 20:41:13 +0100

    :sparkles: Add day1 part2.

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
@@ -21,12 +22,46 @@ fn combine_digit(line: String) -> i32 {
     result.parse::<i32>().unwrap()
 }
 
+fn process_line(line: String) -> String {
+    let mut result: String = String::new();
+    let mut index: usize = 0;
+
+    while index != line.len() {
+        let tmp = &line[index..];
+        if tmp.starts_with("one") {
+            result.push('1');
+        } else if tmp.starts_with("two") {
+            result.push('2');
+        } else if tmp.starts_with("three") {
+            result.push('3');
+        } else if tmp.starts_with("four") {
+            result.push('4');
+        } else if tmp.starts_with("five") {
+            result.push('5');
+        } else if tmp.starts_with("six") {
+            result.push('6');
+        } else if tmp.starts_with("seven") {
+            result.push('7');
+        } else if tmp.starts_with("eight") {
+            result.push('8');
+        } else if tmp.starts_with("nine") {
+            result.push('9');
+        } else {
+            result.push(line.chars().nth(index).unwrap());
+        }
+        index += 1;
+    }
+    return result;
+}
+
 fn main() {
     let lines: Vec<String> = read_lines("input.txt").expect("Can't read file");
     let mut result: i32 = 0;
 
-    for line in lines {
+    for mut line in lines {
+        line = process_line(line);
+        println!("{:?}", line);
         result = result + combine_digit(line);
     }
-    println!("{:?}", result)
+    println!("{:?}", result);
 }
