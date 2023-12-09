2118663afc8e1c63fe76f8ac34f46a31a07217d8
Kamel <kbencherif29@gmail.com>
2023-12-09 22:17:52 +0100

    :sparkles: Add day6 part2.

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -9,7 +9,7 @@ fn read_lines(filename: &str) -> io::Result<Vec<String>> {
 
 fn main() -> io::Result<()> {
     let lines = read_lines("input.txt")?;
-    let (durations, distances): (Vec<i32>, Vec<i32>) = process_input(lines);
+    let (durations, distances): (Vec<i32>, Vec<i32>) = process_input(lines.clone());
     let mut result: Vec<i32> = vec![0; durations.len()];
 
     for (i, duration) in durations.iter().enumerate() {
@@ -22,6 +22,16 @@ fn main() -> io::Result<()> {
     }
 
     println!("result: {:?}", result.iter().product::<i32>());
+
+    let mut result: u64 = 0;
+    let (duration, distance) = process_input_part2(lines);
+    for i in 0..duration + 1 {
+        let tmp: u64 = (duration - i as u64) * i as u64;
+        if tmp > distance {
+            result += 1;
+        }
+    }
+    println!("result: {}", result);
     Ok(())
 }
 
@@ -40,5 +50,31 @@ fn process_input(lines: Vec<String>) -> (Vec<i32>, Vec<i32>) {
             .map(|n| n.parse::<i32>())
             .flatten()
             .collect::<Vec<i32>>(),
+    )
+}
+
+fn process_input_part2(lines: Vec<String>) -> (u64, u64) {
+    let first_line = lines
+        .first()
+        .unwrap()
+        .split_once(":")
+        .unwrap()
+        .1
+        .to_string();
+    let last_line = lines.last().unwrap().split_once(":").unwrap().1.to_string();
+
+    (
+        first_line
+            .chars()
+            .filter(|c| !c.is_whitespace())
+            .collect::<String>()
+            .parse::<u64>()
+            .unwrap(),
+        last_line
+            .chars()
+            .filter(|c| !c.is_whitespace())
+            .collect::<String>()
+            .parse::<u64>()
+            .unwrap(),
     )
 }
